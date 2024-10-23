3b2601889a6b88f46a9069af252420b1478897a6
VachetVirginie <vachetvirginie@gmail.com>
2024-10-23 11:09:18 +0200

    Create disabledSubmit.vue

---
+++ disabledSubmit.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import NirField from '../src/patterns/NirField';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	components: {
+		NirField,
+	},
+	data: () => ({
+		isFormValid: false,
+		value: null,
+	}),
+	methods: {
+		submitForm() {
+			if (this.$refs.form.validate()) {
+				console.log('Form sent');
+			}
+		},
+	},
+});
+</script>
+
+<template>
+	<VForm ref="form" v-model="isFormValid" @submit.prevent="submitForm">
+		<NirField
+			v-model="value"
+			:nir-length="13"
+			required
+			@input="this.$refs.form.validate()"
+		/>
+		<VBtn color="primary" :disabled="!isFormValid" @click="submitForm">Submit</VBtn>
+	</VForm>
+</template>
